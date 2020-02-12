ac3eaabbbd39b2d8ab58cf20f6be22b747c61ec4
Madatt <alter.mail@o2.pl>
2020-02-13 00:37:16 +0100

    ...

--- ffmdtt/main.py
+++ ffmdtt/main.py
@@ -5,21 +5,6 @@ from ffmdtt import parser
 from ffmdtt import mtoken
 from ffmdtt import generator
 
-test_str: str = "def f32 add_mul(f32 arg1; u32 arg2)" \
-                "{" \
-                "u32 c = arg1 * (arg1 + arg2);" \
-                "u32 h = c; " \
-                "if( c ) {" \
-                "u32 f = h + c;" \
-                "c = c + c;" \
-                "if(f) {" \
-                "return f;" \
-                "}" \
-                "}" \
-                "return c;" \
-                "}"
-
-
 test_str2 = open("test.msl").read()
 
 tknz = tokenizer.Tokenizer(test_str2)
